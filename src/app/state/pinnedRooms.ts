c75cd3d52c853b217d3a88cef41ae2b1e0f50c12
Novusbot <ruster91@bk.ru>
2026-06-09 15:28:47 +0500

    feat: add pinnedRooms atom with Matrix RoomEvent.Tags subscription

---
+++ src/app/state/pinnedRooms.ts
@@ -0,0 +1,55 @@
+import { atom, useSetAtom } from 'jotai';
+import { MatrixClient, Room, RoomEvent } from 'matrix-js-sdk';
+import { useEffect } from 'react';
+
+export type PinnedRoomsAction = {
+  type: 'INITIALIZE' | 'UPDATE';
+  rooms: Set<string>;
+};
+
+export const PIN_TAG = 'm.favourite';
+
+const basePinnedRoomsAtom = atom(new Set<string>());
+
+export const pinnedRoomsAtom = atom<Set<string>, [PinnedRoomsAction], undefined>(
+  (get) => get(basePinnedRoomsAtom),
+  (get, set, action) => {
+    set(basePinnedRoomsAtom, action.rooms);
+  }
+);
+
+const collectPinnedRoomIds = (mx: MatrixClient): Set<string> => {
+  const pinnedRoomIds = new Set<string>();
+  mx.getRooms().forEach((room: Room) => {
+    if (room.tags?.[PIN_TAG] !== undefined) {
+      pinnedRoomIds.add(room.roomId);
+    }
+  });
+  return pinnedRoomIds;
+};
+
+export const useBindPinnedRoomsAtom = (
+  mx: MatrixClient,
+  pinnedAtom: typeof pinnedRoomsAtom
+) => {
+  const setPinned = useSetAtom(pinnedAtom);
+
+  useEffect(() => {
+    setPinned({
+      type: 'INITIALIZE',
+      rooms: collectPinnedRoomIds(mx),
+    });
+
+    const handleRoomTags = (_event: any, _room?: Room) => {
+      setPinned({
+        type: 'UPDATE',
+        rooms: collectPinnedRoomIds(mx),
+      });
+    };
+
+    mx.on(RoomEvent.Tags, handleRoomTags);
+    return () => {
+      mx.removeListener(RoomEvent.Tags, handleRoomTags);
+    };
+  }, [mx, setPinned]);
+};
